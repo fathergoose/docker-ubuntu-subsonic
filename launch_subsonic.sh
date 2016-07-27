132d9eb31ff649e42bcbbe94989abf8025c1fa33
YAMAMI Taro <mitarothanken@gmail.com>
2016-07-27 21:49:45 +0900

    Set options

--- launch_subsonic.sh
+++ launch_subsonic.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-${SUBSONIC_HOME}/subsonic.sh --default-music-folder=${SUBSONIC_MUSIC_FOLDER} --default-podcast-folder=${SUBSONIC_PODCAST_FOLDER} --default-playlist-folder=${SUBSONIC_PLAYLIST_FOLDER}
+${SUBSONIC_HOME}/subsonic.sh --port=${SUBSONIC_PORT} --https-port=${SUBSONIC_HTTPS_PORT} --default-music-folder=${SUBSONIC_MUSIC_FOLDER} --default-podcast-folder=${SUBSONIC_PODCAST_FOLDER} --default-playlist-folder=${SUBSONIC_PLAYLIST_FOLDER}
 
 tail -F ${SUBSONIC_HOME}/subsonic_sh.log
